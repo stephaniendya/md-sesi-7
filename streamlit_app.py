8e0afcd5de64d1fb014efbbdccfb97c215a850d8
stephaniendya <nadyastephanieeee@gmail.com>
2025-03-17 14:24:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import joblib
 
+def load_model(filename):
+  model = joblib.load(filename)
+  return model
+
+def predict_with_model(model, user_input):
+  prediction = model.predict([user_input])
+  return prediction[0]
 
 def main():
   st.title('Dermatology Machine Learning')
